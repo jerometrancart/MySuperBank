d08e5b7b41656ed4d50a9b034cb7f112f70f8ab7
jerometrancart <jerome.trancart@gmail.com>
2023-06-18 19:49:00 +0200

    added  withdrawal and deposit method definition

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using MySuperBank;
+
+var account = new BankAccount("<name>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
